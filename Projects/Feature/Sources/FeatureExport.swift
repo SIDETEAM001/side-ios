1ef422fe290e2c0d63661e1a8aab76ee9af96d7b
WONGI HONG <dt95@naver.com>
2023-11-09 14:11:48 +0900

    [MOD] : 오타 수정

--- Projects/Feature/Sources/FeatureExport.swift
+++ Projects/Feature/Sources/FeatureExport.swift
@@ -1,10 +1,2 @@
-//
-//  FeatureExport.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 강민성 on 10/30/23.
-//
-
-
-@_exported import FeatureProfile
-@_exported import FeatureProfileInterface
+@_exported import FeatureSignIn
+@_exported import FeatureSignInInterface
