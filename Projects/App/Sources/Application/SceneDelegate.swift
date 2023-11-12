7d1458794f413a7c3841844fb8035ddd03bf119e
WONGI HONG <dt95@naver.com>
2023-11-12 23:32:58 +0900

    [FEAT] : 회원가입 비밀번호 입력 화면 그리기

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -6,26 +6,33 @@
 //
 
 import UIKit
+import Feature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let scene = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let passwordVC = PasswordViewController()
+        let navigationVC = UINavigationController(rootViewController: passwordVC)
+        
+        window?.rootViewController = navigationVC
+        window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {}
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {}
-
+    
     func sceneWillResignActive(_ scene: UIScene) {}
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {}
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {}
 }
