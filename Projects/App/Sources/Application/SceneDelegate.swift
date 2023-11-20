6e7574cfd7f432a614195a34d4081170c926b89d
WONGI HONG <dt95@naver.com>
2023-11-20 22:13:35 +0900

    [ADD] : Component 추가

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Feature
+import FeatureSignUpInterface
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
@@ -20,7 +21,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowscene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame:UIScreen.main.bounds)
         window?.windowScene = windowscene
+        let practiceVC = PracticeVC()
         
+        window?.rootViewController = practiceVC
         window?.makeKeyAndVisible()
     }
     
