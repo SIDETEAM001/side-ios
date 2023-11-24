7707a26661558be9e1ac087a54fe74b7947dcee1
WONGI HONG <dt95@naver.com>
2023-11-24 15:30:34 +0900

    [FEAT] : Input Componet 수정하고 재생성

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -22,6 +22,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame:UIScreen.main.bounds)
         window?.windowScene = windowscene
         
+        let practicevc = PracticeVC()
+        
+        window?.rootViewController = practicevc
         window?.makeKeyAndVisible()
     }
     
