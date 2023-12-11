b30421aa07ebdd513b03e00c02d50d71d7b17582
duthd3 <duthd3@naver.com>
2023-12-11 17:50:11 +0900

    feature/#87

---
+++ Projects/Feature/Home/Sources/NoCreatedGathering.swift
@@ -0,0 +1,20 @@
+//
+//  NoCreatedGathering.swift
+//  FeatureHome
+//
+//  Created by yoonyeosong on 2023/12/11.
+//
+
+import UIKit
+
+class NoCreatedGathering: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
